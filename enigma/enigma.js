06e28eb32886f8f18c2dd91d60d6c56254c989b3
mbouras <marine.bouras@gmail.com>
2015-03-25 10:27:05 +0100

    Création des instructions pour les 2 méthodes 'encrypt' et 'decrypt' de Enigma : utilisation de la fonction replace()

--- enigma/enigma.js
+++ enigma/enigma.js
@@ -1,9 +1,80 @@
 var Enigma = function (){
 
-    this.encrypt = function(){
-        console.log("encrypt : ca marche");
+    this.encrypt = function(messageToEncrypt){
+        //console.log("encrypt : ca marche");
+        var myString = messageToEncrypt;
+        myString = myString.replace("a", "j");
+        myString = myString.replace("b", "d");
+        myString = myString.replace("c", "w");
+        myString = myString.replace("d", "o");
+        myString = myString.replace("e", "v");
+        myString = myString.replace("f", "a");
+        myString = myString.replace("g", "r");
+        myString = myString.replace("h", "i");
+        myString = myString.replace("i", "g");
+        myString = myString.replace("j", " ");
+        myString = myString.replace("k", "x");
+        myString = myString.replace("l", "u");
+        myString = myString.replace("m", "n");
+        myString = myString.replace("n", "s");
+        myString = myString.replace("o", "y");
+        myString = myString.replace("p", "q");
+        myString = myString.replace("q", "c");
+        myString = myString.replace("r", "p");
+        myString = myString.replace("s", "'");
+        myString = myString.replace("t", "f");
+        myString = myString.replace("u", "m");
+        myString = myString.replace("v", "z");
+        myString = myString.replace("w", "t");
+        myString = myString.replace("x", "k");
+        myString = myString.replace("y", "h");
+        myString = myString.replace("z", "b");
+        myString = myString.replace(" ", "e");
+        myString = myString.replace("'", "l");
+        myString = myString.replace(".", ".");
+        myString = myString.replace(",", ",");
+        myString = myString.replace(":", ":");
+        myString = myString.replace(";", ";");
+
+        return myString;
+
     };   
-    this.decrypt = function(){
-        console.log("decrypt : ça marche aussi");
+    this.decrypt = function(messageToDecrypt){
+        //console.log("decrypt : ça marche aussi");
+        var myString = messageToDecrypt;
+        myString = myString.replace("j", "a");
+        myString = myString.replace("d", "b");
+        myString = myString.replace("w", "c");
+        myString = myString.replace("o", "d");
+        myString = myString.replace("v", "e");
+        myString = myString.replace("a", "f");
+        myString = myString.replace("r", "g");
+        myString = myString.replace("i", "h");
+        myString = myString.replace("g", "i");
+        myString = myString.replace(" ", "j");
+        myString = myString.replace("x", "k");
+        myString = myString.replace("u", "l");
+        myString = myString.replace("n", "m");
+        myString = myString.replace("s", "n");
+        myString = myString.replace("y", "o");
+        myString = myString.replace("q", "p");
+        myString = myString.replace("c", "q");
+        myString = myString.replace("p", "r");
+        myString = myString.replace("'", "s");
+        myString = myString.replace("f", "t");
+        myString = myString.replace("m", "u");
+        myString = myString.replace("z", "v");
+        myString = myString.replace("t", "w");
+        myString = myString.replace("k", "x");
+        myString = myString.replace("h", "y");
+        myString = myString.replace("b", "z");
+        myString = myString.replace("e", " ");
+        myString = myString.replace("l", "'");
+        myString = myString.replace(".", ".");
+        myString = myString.replace(",", ",");
+        myString = myString.replace(":", ":");
+        myString = myString.replace(";", ";");
+
+        return myString;
     };
 };
